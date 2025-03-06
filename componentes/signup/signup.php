7a2bce95a0f4047ae84b08807211e93dce703a4f
Danielcreux <jd2000_fs@hotmail.com>
2025-03-06 22:24:31 +0100

    Primer commit

---
+++ componentes/signup/signup.php
@@ -0,0 +1,14 @@
+   <div id="contienesignup">
+    	<h3>Crea un nuevo usuario</h3>
+    	<input type="text" id="nuevousuario">
+    	<input type="password" id="nuevacontrasena">
+    	<button id="crearusuario">Crea usuario</button>
+    </div>
+
+    
+<style>
+	<?php include "signup.css";?>
+</style>
+<script>
+	<?php include "signup.js";?>
+</script>
